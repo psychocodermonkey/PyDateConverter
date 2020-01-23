52b1930a606e694b335a208f20b41e377cd03dc2
Andrew Dixon <psychocodermonkey@gmail.com>
2020-01-23 11:20:29 -0600

    Update .gitignore. Add docstrings to exceptions.

--- dateCnvExceptions.py
+++ dateCnvExceptions.py
@@ -3,20 +3,20 @@
 #  pass
 
 class FormatMaskNotFound(Exception):
-  # "Format mas not found in valid format mask dictionaries"
-  pass
+  ''' Format mas not found in valid format mask dictionaries '''
+  # pass
 
 
 class InputDateOrFormatMaskInvalid(Exception):
-  # "Format mask/date is invalid"
-  pass
+  ''' Format mask/date is invalid '''
+  # pass
 
 
 class DateOutOfRange(Exception):
-  # "Number passed is bigger than it should be for a valid date"
-  pass
+  ''' Number passed is bigger than it should be for a valid date '''
+  # pass
 
 
 class OutputFormatInvalid(Exception):
-  # "Format mask for output not found"
-  pass
+  ''' Format mask for output not found '''
+  # pass
